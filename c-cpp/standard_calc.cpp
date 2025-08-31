69e30bd45c4c523bc71166bfa4a763a7f12d26a2
jb3982 <banyaljai30397.kk@gmail.com>
2025-08-31 16:49:28 -0700

    Completely implemented the 2 functions

--- c-cpp/standard_calc.cpp
+++ c-cpp/standard_calc.cpp
@@ -1,5 +1,6 @@
 #include "stdbool.h"
 #include "standard_calc.h"
+#include "math.h"
 
 /**
  * @brief Bounds the provided angle between [-180, 180) degrees.
@@ -13,7 +14,29 @@
  * @return float: The bounded angle in degrees.
  */
 float bound_to_180(float angle) {
-    return 0;
+    angle = fmod(angle, 360.0);
+    if (angle >= 180.0) {
+        return angle - 360.0;
+    } else if (angle < -180.0) {
+        return angle + 360.0;
+    }
+    return angle;
+}
+
+
+/**
+ * @brief Normalizes an angle to the range [0, 360) degrees.
+ *
+ * @param angle: The input angle in degrees.
+ *
+ * @return float: The normalized angle in degrees.
+ */
+float normalize_angle(float angle) {
+    angle = fmod(angle, 360.0);
+    if (angle < 0.0) {
+        angle += 360.0;
+    }
+    return angle;
 }
 
 /**
@@ -29,5 +52,21 @@ float bound_to_180(float angle) {
  * @return bool: TRUE when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, FALSE otherwise
  */
 bool is_angle_between(float first_angle, float middle_angle, float second_angle) {
-    return true;
+    float norm_first = normalize_angle(first_angle);
+    float norm_middle = normalize_angle(middle_angle);
+    float norm_second = normalize_angle(second_angle);
+
+    // distance between first and second angle
+    float dist_first_second = fmod(norm_second - norm_first + 360.0, 360.0);
+
+    if (dist_first_second <= 180.0) {
+        // Minor arc is CCW from first to second
+        float dist_first_middle = fmod(norm_middle - norm_first + 360.0, 360.0);
+        return (dist_first_middle <= dist_first_second);
+    } else {
+        // Minor arc is CCW from second to first
+        float minor_arc = 360 - dist_first_second;
+        float dist_second_middle = fmod(norm_middle - norm_second + 360.0, 360.0);
+        return (dist_second_middle <= minor_arc);
+    }
 }
